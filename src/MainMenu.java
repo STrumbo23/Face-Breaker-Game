eeb8f96441bf33a2f622949a2a55ab5e593ee4b9
lagretz <lgretz@eagles.bridgewater.edu>
2016-10-19 14:28:16 -0400

    Basic look of main menu added, no functionality

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,53 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class MainMenu {
+public class MainMenu extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override 
+	public void start(Stage mainStage) {
+		mainStage.setTitle("FaceBreaker Game!");
+		Label title = new Label("FACE BREAKER GAME");
+		title.getStyleClass().add("title");
+		
+		Button playGame = new Button("Click to Play!");
+		Button highScore = new Button("High Scores");
+		Button help = new Button("Help");
+		Button settings = new Button("Settings");
+	
+		VBox buttons = new VBox();
+		buttons.setSpacing(10);
+		buttons.setPadding(new Insets(10, 20, 10, 20)); 
+		buttons.getChildren().addAll(playGame, highScore, help, settings);
+		buttons.getStyleClass().add("buttons");
+
+		StackPane root = new StackPane();
+		StackPane.setAlignment(title, Pos.TOP_CENTER);
+        root.getChildren().addAll(title, buttons);
+        
+        Scene scene = new Scene(root, 800, 500);
+        scene.getStylesheets().add(this.getClass()
+        		.getResource("stylesheet.css").toExternalForm());
+        
+        mainStage.setScene(scene);
+        mainStage.show();
+		
+	}
 
 }
