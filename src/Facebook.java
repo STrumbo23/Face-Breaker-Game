ef78b4898201a32d8113c33de3274d5b25590c6a
TheLoneJohn <jpadilla@eagles.bridgewater.edu>
2016-11-07 14:18:08 -0500

    FBGraph

--- src/Facebook.java
+++ src/Facebook.java
@@ -1,38 +1,68 @@
-public class Facebook {
+package com.javapapers.java.social.facebook;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+
+public class FBGraph {
+	private String accessToken;
+
+	public FBGraph(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getFBGraph() {
+		String graph = null;
+		try {
+
+			String g = "https://graph.facebook.com/me?" + accessToken;
+			URL u = new URL(g);
+			URLConnection c = u.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					c.getInputStream()));
+			String inputLine;
+			StringBuffer b = new StringBuffer();
+			while ((inputLine = in.readLine()) != null)
+				b.append(inputLine + "\n");
+			in.close();
+			graph = b.toString();
+			System.out.println(graph);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR in getting FB graph data. " + e);
+		}
+		return graph;
+	}
+
+	public Map getGraphData(String fbGraph) {
+		Map fbProfile = new HashMap();
+		try {
+			
+			JSONObject json = new JSONObject(fbGraph);
+			fbProfile.put("id", json.getString("id"));
+			fbProfile.put("first_name", json.getString("first_name"));
+			if (json.has("email"))
+				fbProfile.put("email", json.getString("email"));
+			if (json.has("proPic"))
+				fbProfile.put("picture", json.getObject("picture"));
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+			throw new RuntimeException("ERROR in parsing FB graph data. " + e);
+		}
+		return fbProfile;
+	}
+}
 	
-	//this line of code is new
-    // get these from FB Dev App
-    private static final String api_key = "MYAPIKEY";     
-    private static final String secret = "MYSECRETKEY";
-    private static final String client_id = "MYCLIENTID";  
-
-    // set this to the servlet URL for the authentication servlet/filter
-    private static final String redirect_uri = "http://www.onmydoorstep.com.au/fbauth"; 
-    
-    /// set this to the list of extended permissions you want
-    private static final String[] perms = new String[] {"publish_stream", "email"};
-
-    public static String getAPIKey() {
-        return api_key;
-    }
-
-    public static String getSecret() {
-        return secret;
-    }
-
-    /*public static String getLoginRedirectURL() {
-        return "https://graph.facebook.com/oauth/authorize?client_id=" + 
-            client_id + "&display=page&redirect_uri=" + 
-            redirect_uri+"&scope="+StringUtil.delimitObjectsToString(",", perms);
-    }
-*/
-    public static String getAuthURL(String authCode) {
-        return "https://graph.facebook.com/oauth/access_token?client_id=" + 
-            client_id+"&redirect_uri=" + 
-            redirect_uri+"&client_secret="+secret+"&code="+authCode;
-    }
     	    
-    private void importFbProfilePhoto() {
+   /* private void importFbProfilePhoto() {
 	        if (AccessToken.getCurrentAccessToken() != null) 
 	        	{
 	            	GraphRequest request = GraphRequest.newMeRequest (AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() 
@@ -54,4 +84,4 @@ public class Facebook {
 	        }
 	    }
 	}
-}
+}*/
