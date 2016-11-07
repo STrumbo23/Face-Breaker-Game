6395544e06a14e752c81bde7424803406904d598
lagretz <lgretz@eagles.bridgewater.edu>
2016-11-07 14:49:46 -0500

    back button on help does nothing but close

--- src/Help.java
+++ src/Help.java
@@ -3,26 +3,41 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Help extends Application {
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	public void start(Stage mainStage) {
 		mainStage.setTitle("Help");
 		
+		Button back = new Button("Back");
+		back.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				//mainStage.show();
+				//stage.show();
+				mainStage.close();
+			}
+		});
+
+		
 		StringBuilder text = addText();
 		Text help = new Text();
 		help.setText(text.toString());
 		
 		StackPane root = new StackPane();
+		StackPane.setAlignment(back, Pos.TOP_LEFT);
 		Scene scene = new Scene(root, 800, 500);
-		root.getChildren().addAll(help);
+		root.getChildren().addAll(help, back);
 
 		mainStage.setScene(scene);
 	    mainStage.show();
